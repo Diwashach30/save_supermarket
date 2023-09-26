45f46404ed241e215a3c0d4ba0f2c4ebdc8b0011
Diwashach30 <diwashacharya30@gmail.com>
2023-09-27 00:09:01 +0545

    day 14 done

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -1,21 +1,21 @@
 class ProductModel {
-  static final items = [
+  static List<Item> items = [
     Item(
-        id: "surf",
-        name: "Tide",
-        desc: "Best Detergent Powder",
-        price: "185",
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "Apple iPhone 12th generation",
+        price: 190000,
         color: "#33505a",
         image:
-            "https://rukminim2.flixcart.com/image/850/1000/jxjahe80/washing-powder/y/k/d/5-plus-jasmine-rose-tide-original-imafhzfaysmhfmcb.jpeg?q=20")
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
   ];
 }
 
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
-  final String price;
+  final num price;
   final String color;
   final String image;
 
@@ -26,4 +26,22 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+  factory Item.fromMap(Map<dynamic, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"],
+    );
+  }
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "image": image,
+      };
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:save_supermarket/models/products.dart';
 import 'package:save_supermarket/widgets/drawer.dart';
 import 'package:save_supermarket/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    ProductModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => ProductModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Save Supermarket"),
@@ -16,10 +38,10 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: dummyList.length,
+          itemCount: ProductModel.items.length,
           itemBuilder: (context, index) {
             return ItemWidget(
-              item: dummyList[index],
+              item: ProductModel.items[index],
             );
           },
         ),
